7cd3cd156c2c6b0f84c647f82379c7a7bc7069fc
Prince Sharma <sharmaprince2271997@gmail.com>
2019-01-21 23:22:09 +0530

    second commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://prince:prince22@ds161794.mlab.com:61794/devtech'
+  mongoURI: 'mongodb://prince:prince22@ds161794.mlab.com:61794/devtech',
+  secretOrKey: 'secret'
 };
