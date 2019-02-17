0dab2980bde0af123511651155b1d0e1f45756b4
Prince Sharma <sharmaprince2271997@gmail.com>
2019-02-17 18:09:57 +0530

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://prince:prince22@ds161794.mlab.com:61794/devtech',
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
